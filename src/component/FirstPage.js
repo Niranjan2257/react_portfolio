f980351d516a0a9e85388f4ec75af5df8b49dc76
Niranjan <niranjan2257@gmail.com>
2023-05-27 08:47:37 +0530

    first page aligned

--- src/component/FirstPage.js
+++ src/component/FirstPage.js
@@ -4,14 +4,17 @@ import profile from './image/profile.jpg'
 export default function FirstPage() {
   return (
     <div className="profile_page" id="home">
-    <div className='prfile_page1'>
-        <button classname="openbtn" onclick="openNav()">☰</button>
-        <img src={profile} className="avatar" alt='profile'/></div>
-    <div className='prfile_page2'>
+      <button classname="openbtn" onclick="openNav()">☰</button>
+      <div className='profile'>
+        <div>
+            <img src={profile} className="avatar" alt='profile'/></div>
+        <div>
         <div classname="profile_text"><center><h1>NIRANJAN KUMAR</h1>
-            <p>COMPUTER SCIENCE ENGINEER</p>
-            <button className="resume"><a href="./Resume.pdf" download>DOWNLOAD CV</a></button></center>
+                <p>COMPUTER SCIENCE ENGINEER</p>
+                <button className="resume"><a href="./Resume.pdf" download>DOWNLOAD CV</a></button></center>
         </div>
+      </div>
+      
     </div>                
 </div>
   )
